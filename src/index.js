da5afd7fc23eb1067ffa96180da4aa7cd1ed2d68
jeffrey <jeffreyon11@gmail.com>
2022-08-11 11:49:06 -0700

    setup live reload

--- src/index.js
+++ src/index.js
@@ -1,35 +1,3 @@
-import { Document, Packer} from '../node_modules/docx';
-import {saveAs} from '../node_modules/file-saver';
-
-
-
-
-let templates = {};
-
-
-
-
-
-document.getElementById('generate').addEventListener('click', function(e){
-    // generate a word template using a template function that takes in a resume object
-    console.log('hehe')
-    generateWordDocument();
-})
-
-function generateWordDocument(){
-    let doc = new Document({
-        sections: []
-    })
-
-    saveDoc(doc, "New file.docx")
-}
-
-function saveDoc(doc, fileName){
-    const mimeType = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-    // Create a Blob object from Packer containing the Document instance and the mimeType
-    Packer.toBlob(doc).then((blob) => {
-      const docblob = blob.slice(0, blob.size, mimeType)
-      // Save the file using saveAs from the file-saver package
-      saveAs(docblob, fileName)
-    })
-}
+import Docxtemplater from "docxtemplater";
+import { saveAs } from "file-saver";
+import "bootstrap/dist/css/bootstrap.css";
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+let path = require('path');
+
+module.exports = {
+    mode: 'development', // best place to add env variable checking
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        static: './',
+        hot: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html'
+        })
+    ]
+}
